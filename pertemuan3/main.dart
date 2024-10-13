a2a1992836441375aca564450476393f008c9c33
SugihS <sugihsahyudi417@gmail.com>
2024-10-13 17:52:33 +0700

    Update main.dart

--- pertemuan3/main.dart
+++ pertemuan3/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pertemuan_5/components/konten.dart';
+import 'package:pertemuan_3/components/konten.dart';
 
 void main() {
   runApp(const MyApp());
